583f41886a95aeff785dc2b43e4d5f9dc15857f3
detrone <vutquynh24@gmail.com>
2025-04-05 04:35:38 -0700

    chatroom

---
+++ frontend/scam-me/src/app/map/page.js
@@ -0,0 +1,12 @@
+import Layout from '../components/layout.js';
+
+const ExplorePage = () => {
+  return (
+    <Layout>
+      <h1>Explore</h1>
+      <p>daniel is hated</p>
+    </Layout>
+  );
+};
+
+export default ExplorePage;
